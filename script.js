cab23690e1e96bf8ee5cb1faa4eb07bb5cca5d46
gitgodmod <157794063+gitgodmod@users.noreply.github.com>
2024-02-04 21:11:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.write("heelo guyz");
+document.write("   yo guyz");
+
+var a = 20;
+var b = 10;
+var c = a+b;
+document.write("sum of 20 and 10 is : ", c);
+document.write("sum of 20 and 10 is : ", c);
+document.write("sum of 20 and 10 is : ", c);
+document.write("sum of 20 and 10 is : ", a);
+
+
+
